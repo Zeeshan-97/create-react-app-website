65221ef2a253fb276ffc6f676404ac1557af50e6
Zeeshan-97 <69589055+Zeeshan-97@users.noreply.github.com>
2023-04-09 03:31:26 +0500

    components created

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Theme from './theme/Theme';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './theme/GlobalStyle';
+import { Provider } from 'react-redux';
+import { store } from './state/store/store'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <Provider store={store}>
+        <App />
+        </Provider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
